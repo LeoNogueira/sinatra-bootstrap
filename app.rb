1f0b46125efd00f41804b4ad94c8dcf324bb340e
Leonardo Nogueira <leonardo@ntec.com.br>
2012-03-20 08:02:45 -0300

    Initial setup

---
+++ app.rb
@@ -0,0 +1,4 @@
+require "./config/boot"
+
+class MyApp < Sinatra::Application
+end
---
+++ config/boot.rb
@@ -0,0 +1,18 @@
+require "bundler"
+Bundler.setup(:default, ENV.fetch("RACK_ENV", :development))
+Bundler.require
+
+require File.expand_path("../environment", __FILE__)
+
+# Add directories to load path.
+#
+$LOAD_PATH.tap do |path|
+  path << File.expand_path("../../app", __FILE__)
+end
+
+# Load all initializers. Files will be loaded in
+# alphabetical order.
+#
+Dir[File.dirname(__FILE__) + "/initializers/**/*.rb"].each do |file|
+  require file
+end
